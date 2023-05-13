1a5bccde211ef8cc812cfc73b53f0d3463c10a07
ctruo <ctruongg67@gmail.com>
2023-05-12 23:25:35 -0700

    Added hover effects

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,10 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav>
-      <img id="logo" src="/logo.png" alt="Hoopstagram logo"></img>
+      <a href="/">
+        <img id="logo" src="/logo.png" alt="Hoopstagram logo"></img>
+      </a>
+
       <div id="nav-links">
         <a href="/">Feed</a>
         <a href="/">Games</a>
