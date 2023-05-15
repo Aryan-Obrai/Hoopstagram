76151a8b93570028f5db52c19f847fa9ddf6a2da
ctruo <ctruongg67@gmail.com>
2023-05-14 22:10:34 -0700

    Landing graphic done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,12 @@
 import "./Home.css";
+import LandingGraphic from "./LandingGraphic";
 
 function Home() {
   return (
     <div>
-      <div id="main">
-        <div id="main-text">
-          <h1>All Things</h1>
-          <img id="NBA-logo" src="NBA-logo.png" alt="NBA logo"></img>
-          <div>
-            <p>Keep up with the latest NBA hot</p>
-            <p>topics, hype, and moments 🔥</p>
-          </div>
-          <h3>All-Time Fandom</h3>
-        </div>
-      </div>
+      <section id="landing">
+        <LandingGraphic />
+      </section>
     </div>
   );
 }
