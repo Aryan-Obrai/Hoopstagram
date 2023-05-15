30af58da4303edbd9171174e99935f32ee8184ec
ctruo <ctruongg67@gmail.com>
2023-05-14 19:46:51 -0700

    Routing main pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Feed from "./pages/Feed/Feed";
+import Games from "./pages/Games/Games";
+import TeamsPlayers from "./pages/TeamsPlayers/TeamsPlayers";
 
 function App() {
   return (
     <div>
-      <header>
-        <Navbar />
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/games" element={<Games />} />
+        <Route path="/teams+players" element={<TeamsPlayers />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,26 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav>
-      <a href="/">
-        <img id="logo" src="/logo.png" alt="Hoopstagram logo"></img>
-      </a>
+      <Link id="logo" to="/">
+        <img src="/logo.png" alt="Hoopstagram logo"></img>
+      </Link>
 
       <div id="container">
-        <div id="nav-links">
-          <a href="/">Feed</a>
-          <a href="/">Games</a>
-          <a href="/">Teams & Players</a>
-        </div>
+        <ul id="nav-links">
+          <li>
+            <Link to="/feed">Feed</Link>
+          </li>
+          <li>
+            <Link to="/games">Games</Link>
+          </li>
+
+          <li>
+            <Link to="/teams+players">Teams & Players</Link>
+          </li>
+        </ul>
         <div id="account-btns">
           <button>Sign-up</button>
           <button>Login</button>
