2266a978eaadbd4adf9af8723b7c58f1bfbfa0a0
Aryan-Obrai <hobra001@ucr.edu>
2023-06-12 06:15:38 -0700

    Added Card Flipping Logic

--- frontend/src/components/TeamPage.js
+++ frontend/src/components/TeamPage.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TeamPage.css";
 
 const TeamPage = ({ data }) => {
+  const [flippedCards, setFlippedCards] = useState([]);
+
   if (!data) {
     return null; // Return null or a loading indicator when data is null
   }
 
+  const handleCardClick = (event, index) => {
+    event.stopPropagation();
+    if (flippedCards.includes(index)) {
+      setFlippedCards(flippedCards.filter((card) => card !== index));
+    } else {
+      setFlippedCards([...flippedCards, index]);
+    }
+  };
+
   return (
     <div className="team-page">
-      {/* <h1>TeamPage</h1> */}
       <div className="roster">
-        {/* <h2>Roster</h2> */}
         <div className="team-roster">
-          {data.roster.map((player) => (
-            <div key={player.tid} className="player-box">
-              <img className="player-images" src={player.playerIMG} alt="Profile" />
-              <div className="player-text">
-                <span className="player-name">{player.name}</span>
-                <br></br>
-                {/* <span className="player-jersey"> Jersey Number: {player.jerseyNumber}</span>
-                <span className="player-weight"> Weight: {player.weight}lbs</span>
-                <span className="player-height"> Height: {player.height}</span> */}
-                <span className="player-position"> Postion: {player.position}</span>
+          {data.roster.map((player, index) => (
+            <div
+              key={player.tid}
+              className={`player-box ${flippedCards.includes(index) ? "flipped" : ""}`}
+            >
+              <div className="card" onClick={(event) => handleCardClick(event, index)}>
+                <div className="card-inner">
+                  <div className="front">
+                    <img className="player-images" src={player.playerIMG} alt="Profile" />
+                    <div className="player-text">
+                      <span className="player-name">{player.name}</span>
+                      <br />
+                      <span className="player-position">Position: {player.position}</span>
+                    </div>
+                  </div>
+                  <div className="back">
+                    <span className="player-jersey">Jersey Number: {player.jerseyNumber}</span>
+                    <span className="player-weight">Weight: {player.weight}lbs</span>
+                    <span className="player-height">Height: {player.height}</span>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
